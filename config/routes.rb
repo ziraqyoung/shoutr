3a45512c2e061fa84785527ca20477e47cbd1271
ziraq young <ziraqyoung@outlook.com>
2021-04-19 21:27:00 +0300

    Redirect logged users to separate dashboard separate for non-logged in
    - Generate Dashboard controller with show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  root to: "home#show"
-  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
-  resource :session, only: [:create]
+  constraints Clearance::Constraints::SignedIn.new do
+    root to: 'dashboard#show', as: :dashboard
+  end
+  root to: 'home#show'
 
+  resources :passwords, controller: 'clearance/passwords', only: [:create, :new]
+  resource :session, only: [:create]
   resources :users, only: [:create] do
     resource :password,
-      controller: "clearance/passwords",
+      controller: 'clearance/passwords',
       only: [:edit, :update]
   end
 
-  get "/sign_in" => "sessions#new", as: "sign_in"
-  delete "/sign_out" => "sessions#destroy", as: "sign_out"
-  get "/sign_up" => "users#new", as: "sign_up"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/sign_in' => 'sessions#new', as: 'sign_in'
+  delete '/sign_out' => 'sessions#destroy', as: 'sign_out'
+  get '/sign_up' => 'users#new', as: 'sign_up'
 end
