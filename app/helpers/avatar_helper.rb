5a42a40931556d0fbfeb7abba46d0c25828daa35
ziraq young <ziraqyoung@outlook.com>
2021-04-22 12:48:54 +0300

    Render avatar on users show page
    - Move ShoutsHelper#avatar to AvatarHelper
    - Display it on user's show page

---
+++ app/helpers/avatar_helper.rb
@@ -0,0 +1,8 @@
+module AvatarHelper
+  def avatar(user)
+    email_digest = Digest::MD5.hexdigest(user.email)
+    gravatar_url = "//www.gravatar.com/avatar/#{email_digest}?s=50&d=identicon"
+
+    image_tag  gravatar_url
+  end
+end
--- app/helpers/shouts_helper.rb
+++ app/helpers/shouts_helper.rb
@@ -1,11 +1,4 @@
 module ShoutsHelper
-  def avatar(user)
-    email_digest = Digest::MD5.hexdigest(user.email)
-    gravatar_url = "//www.gravatar.com/avatar/#{email_digest}?s=50&d=identicon"
-
-    image_tag  gravatar_url
-  end
-
   def like_button(shout)
     if  current_user.liked?(shout)
       link_to 'Unlike', unlike_shout_path(shout), method: :delete
