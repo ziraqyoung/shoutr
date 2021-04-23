52e75d4bafdb2ae3bcd1c179cd1e48ef2d61814d
ziraq young <ziraqyoung@outlook.com>
2021-04-23 10:01:34 +0300

    Add counter cache for followers and followed
    - Generate migration to add `followed_users_count` & `followers_count`
        to users table
    - Add `counter_cache` option to FollowingRelationship model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_22_103852) do
+ActiveRecord::Schema.define(version: 2021_04_23_065252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,8 @@ ActiveRecord::Schema.define(version: 2021_04_22_103852) do
     t.string "confirmation_token", limit: 128
     t.string "remember_token", limit: 128, null: false
     t.string "username", null: false
+    t.integer "followed_users_count", default: 0, null: false
+    t.integer "followers_count", default: 0, null: false
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_token"], name: "index_users_on_remember_token"
     t.index ["username"], name: "index_users_on_username", unique: true
