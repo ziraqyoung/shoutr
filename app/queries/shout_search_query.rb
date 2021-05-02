0b925d7e4aac5d12c4aa92fa89ce7518f711cdbf
ziraq young <ziraqyoung@outlook.com>
2021-05-02 08:38:07 +0300

    Allowing photoshouts via attachments filename
    ActiveStorage stores attachments details in `active_storage_attachments`
    table while storing blob information in `active_storage_blobs` table. To
    return Shout objects while reaching across both table requires below
    code
    
    This allows to get ids of shouts by chaining `ids`
    
    ```ruby
     PhotoShout.joins(:image_attachment).joins(:image_blob).where("active_storage_blobs.filename ILIKE ?", "%#{term}%")
    ```
    
    - Modify ShoutSearchQuery to search in both text_shouts and photo_shouts
      tables

--- app/queries/shout_search_query.rb
+++ app/queries/shout_search_query.rb
@@ -4,12 +4,27 @@ class ShoutSearchQuery
   end
 
   def to_relation
-    Shout.
-      joins("LEFT JOIN text_shouts ON content_type = 'TextShout' AND content_id = text_shouts.id").
-      where("text_shouts.body ILIKE ?", "%#{term}%")
+    matching_shouts_for_text_shouts.or(matching_shouts_for_photo_shouts)
   end
 
   private
 
     attr_reader :term
+
+    def matching_shouts_for_text_shouts
+      Shout.where(content_type: "TextShout", content_id: matching_text_shouts.ids)
+    end
+
+    def matching_text_shouts
+      TextShout.where("body ILIKE ?", "%#{term}%")
+    end
+
+    def matching_shouts_for_photo_shouts
+      Shout.where(content_type: "PhotoShout", content_id: matching_photo_shouts.ids)
+    end
+
+    def matching_photo_shouts
+      # this might need refactoring ()
+      PhotoShout.joins(:image_attachment).joins(:image_blob).where("active_storage_blobs.filename ILIKE ?", "%#{term}%")
+    end
 end
